a09033b52127d0ffa81ea81ddcd865217c733fd8
sntpwbl <joaop401@outlook.com>
2025-01-12 20:18:31 -0300

    style: fix mobile display of extended product image

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -16,7 +16,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={lexend.className}>
+      <body className={lexend.className} style={{marginBottom: 10}}>
         <Header />
         {children}
       </body>

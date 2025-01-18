f9efea4286ef0963043ecd420932d569a364dfb6
sntpwbl <joaop401@outlook.com>
2025-01-18 06:45:12 -0300

    fix: added trimright to formatted products name, todo: add in another components which format the products name

--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -6,7 +6,7 @@ import {BoughtLabel} from '../boughtLabel/BoughtLabel'
 
 export default function Product({product, ...rest}){
     const formattedDescription = !product.description || product.description?.length==0 ? '' : product.description.length >= 80 ? product.description.substring(0, 80)+'...' : product.description
-    const formattedName = product.name.length>=15 ? product.name.substring(0, 15)+'...' : product.name
+    const formattedName = product.name.length>=15 ? product.name.substring(0, 15).trimRight()+'...' : product.name
 
     return <Link href={`/produto/${product.id}`} {...rest}>
                 <div style={{border: product.bought ? 'solid 5px #3083FF' : 'none', padding: product.bought ? 15 : 20}} className={styles.productContainer}>
